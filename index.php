deec20ad282aece35d7b944807f35af29e403a42
Annie Kyles <anniek@federation.net.nz>
2022-08-04 13:28:29 +1200

    Design tweaks.

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 <body>
     <div class="container">
         <header>
-            <a class="logo logo-tsb" href="https://federation.net.nz" target="_blank">
+            <a class="logo logo-tsb" href="https://www.tsb.co.nz/" target="_blank">
                 <img src="./images/logos/tsb.svg" alt="TSB logo" alt="TSB logo" title="TSB">
             </a>
             <a class="logo logo-fed" href="https://federation.net.nz" target="_blank">
@@ -28,7 +28,7 @@
             <section class="fade-in">
                 <div class="content">
                     <h1>Kia ora</h1>
-                    <p class="intro">These are exciting times for TSB. As the banking world gets more complicated, there is a real opportunity for you to challenge the conventions of the category. Below is a thought-provoking piece on the power of simplifying banking.</p>
+                    <p class="intro">These are exciting times for TSB. As the banking world gets more complicated, there is a real opportunity for you to challenge the conventions of the category. Below is a thought-provoking piece on the power of simplifying banking. </p>
                 </div>
             </section>
             <section class="fade-in">
@@ -41,8 +41,8 @@
             </section>
             <section class="fade-in">
                 <div class="content">
-                    <h2>Strategy</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, velit mollitia explicabo ipsum est aliquid numquam eaque, facere illo aperiam optio aut repudiandae dolorum magni corrupti iusto cupiditate minima expedita.</p>                
+                    <h2>The new challenger of banking</h2>
+                    <p>We believe we can transform TSB into a power player in New Zealand banking. Read on for our thoughts on building a brand platform that comes to life from the inside and out, one that elevates 'easy’ into an ownable, emotional and challenger brand space.</p>                
                     <a class="button" href="./pdfs/tsb_pitchdoc_strategy_web.pdf" target="_blank" title="">Download strategy
                         
                         <svg version="1.1" id="Isolation_Mode" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px"
@@ -55,7 +55,7 @@
             <section class="fade-in">
                 <div class="content">
                     <h2>We believe in the genius of simple</h2>
-                    <p>The simple solutions are always the best. Chipping away the things that aren’t essential can make your customers lives easier. But simple requires a commitment to doing different. Simple requires smarts. Below is a body of work that talks to the value of being brilliant and simple. </p>                
+                    <p>The simple solutions are always the best. Chipping away the things that aren’t essential can make your customers lives easier. But simple requires a commitment to doing different. Simple requires smarts. Below is a body of work that talks to the value of being brilliant and simple.</p>                
                     <a class="button" href="./pdfs/tsb_presso_creative_book_web.pdf" target="_blank" title="">Download creative
                         <svg version="1.1" id="Isolation_Mode" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px"
                                 y="0px" viewBox="0 0 369.6 532" style="enable-background:new 0 0 369.6 532;" xml:space="preserve">
@@ -68,7 +68,7 @@
             <section class="fade-in">
                 <div class="content">
                     <p class="h1">Ngā mihi nui</p>
-                    <p><a href="https://federation.net.nz/">Sharon, Tony and the team</a> would love the chance to collaborate with your team to build an enduring brand positioning and platform. We look forward to talking more.</p>
+                    <p><a href="https://federation.net.nz/" target="_blank">Sharon, Tony and the team</a> would love the chance to collaborate with you to build an enduring brand positioning and platform. We look forward to talking more.</p>
                 </div>
             </section>
         </div>
